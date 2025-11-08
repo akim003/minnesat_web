a67f8ba1f5a24cd0e4d10f3c11d9121613e079be
A. Kim <kim01598@umn.edu>
2025-11-07 21:27:37 -0600

    Update contact email

--- app/page.tsx
+++ app/page.tsx
@@ -326,7 +326,7 @@ export default function Home() {
                 <div className="space-y-4">
                   <div className="flex items-center">
                     <Mail className="h-5 w-5 text-gold mr-3" />
-                    <span className="text-white/80">umn.minnesat@gmail.com</span>
+                    <span className="text-white/80">wooll052@umn.edu </span>
                   </div>
                   <div className="flex items-center">
                     <svg
